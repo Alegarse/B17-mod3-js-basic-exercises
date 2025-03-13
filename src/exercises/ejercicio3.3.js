0e32cef7033d14eb4b5f5ac99be7ff1aa9d1652c
Alejandro García Serrano <agscm2@gmail.com>
2025-03-13 17:52:40 +0100

    translations

--- src/exercises/ejercicio3.3.js
+++ src/exercises/ejercicio3.3.js
@@ -1,14 +1,14 @@
 function contarVocales(string) {
-    const vocales = "aeiouAEIOU";
+    const vowels = "aeiouAEIOU";
     let arrayString = string.split('');
     let acc = 0;
     arrayString.forEach(char => {
-        if(vocales.includes(char)) acc++;
+        if(vowels.includes(char)) acc++;
     });
-    console.log(`The string "${string}" contains ${acc} vocals`);
+    console.log(`The string "${string}" contains ${acc} vowels`);
 }
 
 export function main3_3() {
-    const string = prompt("Enter a string for vocals's count:", 'Ambiciosa combinacion de colores');
+    const string = prompt("Enter a string for vowels count:", 'Ambiciosa combinacion de colores');
     contarVocales(string);
 }
